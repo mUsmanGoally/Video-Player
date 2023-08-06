7c50cb0888920d26ce7079b97e461460a2d18ba0
Musman <musman@goally.co>
2023-08-06 11:56:27 +0500

    video list issue fix

--- app/src/main/java/com/example/videoplayer/fragments/PlayerFragment.kt
+++ app/src/main/java/com/example/videoplayer/fragments/PlayerFragment.kt
@@ -1,6 +1,10 @@
 package com.example.videoplayer.fragments
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,14 +12,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.videoplayer.R
+import com.example.videoplayer.activities.MainActivity
 import com.example.videoplayer.databinding.FragmentPlayerBinding
+import com.example.videoplayer.models.VideoModel
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import java.io.File
 
 class PlayerFragment : Fragment() {
     private lateinit var binding: FragmentPlayerBinding
     private val args: PlayerFragmentArgs by navArgs()
     private lateinit var player: ExoPlayer
+    private var videoList: ArrayList<VideoModel> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +37,12 @@ class PlayerFragment : Fragment() {
 
     private fun init() {
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        videoList = if (args.position != -1) {
+            getAllVideos(MainActivity.foldersList[args.position].id)
+        } else MainActivity.videoList
+
+        Log.d("TAG", "init: ${videoList.size}")
+
         binding.tvVideoTitle.text = args.videoData.title
         binding.tvVideoTitle.isSelected = true
         createPlayer()
@@ -68,6 +82,65 @@ class PlayerFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         player.release()
+    }
+
+    @SuppressLint("Recycle", "Range", "NotifyDataSetChanged")
+    fun getAllVideos(folderId: String): ArrayList<VideoModel> {
+        val videoList: ArrayList<VideoModel> = ArrayList()
+
+        val selection = MediaStore.Video.Media.BUCKET_ID + " like? "
+        val projection = arrayOf(
+            MediaStore.Video.Media.TITLE,
+            MediaStore.Video.Media.SIZE,
+            MediaStore.Video.Media._ID,
+            MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
+            MediaStore.Video.Media.BUCKET_ID,
+            MediaStore.Video.Media.DATA,
+            MediaStore.Video.Media.DATE_ADDED,
+            MediaStore.Video.Media.DURATION
+        )
+
+        val cursor = requireActivity().contentResolver.query(
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            arrayOf(folderId),
+            MediaStore.Video.Media.DATE_ADDED + " DESC"
+        )
+
+        cursor?.use { itCursor ->
+            if (itCursor.moveToNext()) {
+                do {
+                    val title =
+                        itCursor.getString(itCursor.getColumnIndex(MediaStore.Video.Media.TITLE))
+                    val size =
+                        itCursor.getString(itCursor.getColumnIndex(MediaStore.Video.Media.SIZE))
+                    val id =
+                        itCursor.getString(itCursor.getColumnIndex(MediaStore.Video.Media._ID))
+                    val folderName =
+                        itCursor.getString(itCursor.getColumnIndex(MediaStore.Video.Media.BUCKET_DISPLAY_NAME))
+                    val path =
+                        itCursor.getString(itCursor.getColumnIndex(MediaStore.Video.Media.DATA))
+                    val duration =
+                        itCursor.getString(itCursor.getColumnIndex(MediaStore.Video.Media.DURATION))
+                            .toLong()
+
+                    try {
+                        val file = File(path)
+                        val uri = Uri.fromFile(file)
+                        val video = VideoModel(id, title, duration, folderName, size, path, uri)
+
+                        if (file.exists())
+                            videoList.add(video)
+
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                } while (itCursor.moveToNext())
+            }
+        }
+        return videoList
     }
 
 }
